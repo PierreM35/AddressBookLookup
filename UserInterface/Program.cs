ca6f10646b37cd30d76fb122a5e2fbeb1d6c185c
menar <menardp21@gmail.com>
2024-10-04 18:54:59 +0200

    Remove greeter template

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -4,12 +4,8 @@ using Grpc.Net.Client;
 using Protos;
 
 var channel = GrpcChannel.ForAddress("http://localhost:5091");
-
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "Tim" });
-Console.WriteLine(reply.Message);
-
 var addressBookClient = new AddressBookLookup.AddressBookLookupClient(channel);
+
 var addressBook = addressBookClient.GetAddressBook(new GetAddressBookRequest());
 
 var cancellationtokensource = new CancellationTokenSource();
@@ -18,7 +14,6 @@ var getPersonRequest = new GetPersonsRequest
     Person = new Person { Name = "Pierre" }, 
     FieldMask = new FieldMask { Paths = { "name" } }
 };
-
 using (var call = addressBookClient.GetPersons(getPersonRequest))
 {
     while (await call.ResponseStream.MoveNext(cancellationtokensource.Token))
